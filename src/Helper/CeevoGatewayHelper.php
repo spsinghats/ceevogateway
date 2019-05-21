72eb7c26ca82b4c664d95cc664f029bcd1efc15b
vaibhav <vaibhav.sharma@athenasoft.in>
2019-05-21 11:05:41 +0530

    ceevo gateway

---
+++ src/Helper/CeevoGatewayHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CeevoGateway\Helper;
+
+class CeevoGatewayHelper
+{
+    public function __construct()
+    {
+    }
+
+    public function getMop()
+    {
+        return 1;
+    }
+
+}
